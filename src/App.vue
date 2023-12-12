02404f39175fb72baa79bf0312a9d8b6e2eedc35
AnatakuAmeerah <anatakuameerah@gmail.com>
2023-12-12 11:47:32 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+
+
+<template>
+  <div>
+     <h2>The Date is: {{ dateFormatted }}</h2>
+     <div class="btn">
+     <button @click="addDay(1)">Adds one day</button>
+     <button @click="addMonth(1)">Adds one month</button>
+    </div>
+  </div>
+ </template>
+ 
+ <script>
+ import { ref, computed } from 'vue';
+ export default {
+  setup() {
+     const date = ref(new Date());
+ 
+     const addDay = (days) => {
+       date.value = new Date(date.value.getTime() + days * 24 * 60 * 60 * 1000);
+     };
+ 
+     const addMonth = (months) => {
+       date.value = new Date(date.value.getFullYear(), date.value.getMonth() + months, date.value.getDate());
+     };
+ 
+     const dateFormatted = computed(() => date.value.toLocaleDateString());
+ 
+     return { dateFormatted, addDay, addMonth };
+  },
+ };
+ </script> 
+
+
+
+
